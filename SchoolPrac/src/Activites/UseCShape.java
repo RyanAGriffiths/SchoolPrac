5fa13af6c46c1915da705dea4136707c4bbfe422
Ryan Griffiths <ragriffiths99@gmail.com>
2021-04-18 11:54:21 +0200

    Exercise 1 - page 63. Line object. 4/18/21

---
+++ SchoolPrac/src/Activites/UseCShape.java
@@ -0,0 +1,14 @@
+package Activites;
+
+public class UseCShape {
+     public static void main(String[] args) {
+        CShape stripe = new CShape();
+        CShapeHorizontal stripe2 = new CShapeHorizontal();
+        stripe2.draw();
+        stripe.draw();
+        stripe.draw();
+        stripe.draw();
+        stripe.draw();
+        stripe2.draw();
+    }
+}
