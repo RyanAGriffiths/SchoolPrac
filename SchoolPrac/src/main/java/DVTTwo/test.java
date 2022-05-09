597a4ac4b98d50a9c53d1642947621259de4db69
Ryang <Ryang@ZAKZRHB-CLB23.ballito.local>
2022-05-09 15:36:28 +0200

    dvt two

---
+++ SchoolPrac/src/main/java/DVTTwo/test.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DVTTwo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author ryang
+ */
+public class test {
+
+    public static void main(String[] args) {
+        /*
+        Date d = new Date();
+        Appointment a = new Appointment("Ryan", "G", "0405136282085","04/05/13", true, 17);
+        a.checkAge("0405136282085");
+         */
+        AppointmentArray a = new AppointmentArray();
+        a.toString();
+    }
+
+}
